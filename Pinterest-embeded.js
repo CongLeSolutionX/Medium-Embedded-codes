23ca56b90748070e4801876e1181d2d60612e406
Cong Le <longchike@gmail.com>
2024-08-18 06:17:25 -0700

    - add code

---
+++ Pinterest-embeded.js
@@ -0,0 +1,15 @@
+<iframe title="I Asked AI Bots Pinterest account link">
+  <body>
+    <script type="text/javascript" async defer src="//assets.pinterest.com/js/pinit.js"></script>
+
+    <a aria-label="IAskedAIBots Pinterest"
+      href="https://www.pinterest.com/IAskedAIBots/"
+      data-pin-do="embedUser"
+      data-pin-board-width="400"
+      data-pin-scale-height="320"
+      data-pin-scale-width="80">
+    </a>
+
+    <script async defer src="//assets.pinterest.com/js/pinit.js"></script>
+  </body>
+</iframe>
